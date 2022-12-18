550d4ac74394cd316458a8974d83dfaac881b6a7
Sree Tej Peddaiahgari <sreetejpeddaiahgari@gmail.com>
2022-12-18 10:30:19 -0600

    adding Map View

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by Sree Tej Peddaiahgari on 12/18/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 36.1627, longitude: -86.7816), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    var body: some View {
+        VStack {
+            Map(coordinateRegion: $region)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Sree Tej Peddaiahgari on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
